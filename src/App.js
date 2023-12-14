6a4596edad95ca0961b8f47678509e2dbf61c36a
ChandupaSH <chandupash107@gmail.com>
2023-12-14 17:28:26 +0530

    remove router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {  HashRouter as Router, Route, Routes } from 'react-router-dom'
-import HomePage from './scenes/HomePage.jsx'
+// import HomePage from './scenes/HomePage.jsx'
 
 const App = () => {
   return (
-    
-      <Router>
-        <Routes>
-          <Route path='/' element = { <HomePage/>} />
-        </Routes>
-      </Router> 
+    <div>
+      <h1>app.js</h1>
+    </div>
+      // <Router>
+      //   <Routes>
+      //     <Route path='/' element = { <HomePage/>} />
+      //   </Routes>
+      // </Router> 
   )
 }
 
