0c8ec5753135eed7fd058b875f13dbde47f53cce
ChandupaSH <chandupash107@gmail.com>
2023-12-14 16:02:52 +0530

    type module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import {  HashRouter as Router, Route, Routes } from 'react-router-dom'
-import HomePage from './scenes/HomePage'
+import HomePage from './scenes/HomePage.jsx'
 
 const App = () => {
   return (
     
       <Router>
         <Routes>
-          <Route path="/" element = { <HomePage/>} />
+          <Route path='/' element = { <HomePage/>} />
         </Routes>
-      </Router>
-     
-
-    
+      </Router> 
   )
 }
 
