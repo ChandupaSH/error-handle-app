d46f90f43b591a442285c1153f89ebe01e2d220c
ChandupaSH <chandupash107@gmail.com>
2023-12-14 20:44:54 +0530

    sgsd

--- src/scenes/HomePage.jsx
+++ src/scenes/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>HomePage</h1>
+      <button onClick={()=>{navigate('/user')}}>User Page</button>
     </div>
   )
 }
