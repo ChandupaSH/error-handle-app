1253fea23ff25048d99c4be6f16e05971d5b1e4e
ChandupaSH <chandupash107@gmail.com>
2023-12-14 20:24:57 +0530

    zdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {  HashRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './scenes/HomePage.jsx'
+import UserPage from './scenes/UserPage.jsx'
 
 const App = () => {
   return (
       <Router>
         <Routes>
           <Route path='/' element = { <HomePage/>} />
+          <Route path='/user' element = { <UserPage/>} />
         </Routes>
       </Router> 
   )
---
+++ src/scenes/UserPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>
+      <span>User Page</span>
+    </div>
+  )
+}
+
+export default UserPage
