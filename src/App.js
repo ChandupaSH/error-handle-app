0828b131fc7d84a7aa069c7438c88e17dd2b7518
ChandupaSH <chandupash107@gmail.com>
2023-12-14 17:49:08 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import {  HashRouter as Router, Route, Routes } from 'react-router-dom'
-// import HomePage from './scenes/HomePage.jsx'
+import HomePage from './scenes/HomePage.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1>app.js</h1>
-    </div>
-      // <Router>
-      //   <Routes>
-      //     <Route path='/' element = { <HomePage/>} />
-      //   </Routes>
-      // </Router> 
+      <Router>
+        <Routes>
+          <Route path='/' element = { <HomePage/>} />
+        </Routes>
+      </Router> 
   )
 }
 
