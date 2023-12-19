151704e86d83360f4b46b85aeddb0fcb551ea3ea
ChandupaSH <chandupash107@gmail.com>
2023-12-19 21:06:14 +0530

    guouh

--- src/components/Navlanding.jsx
+++ src/components/Navlanding.jsx
@@ -1,4 +1,4 @@
-import { IconButton, useMediaQuery } from '@mui/material'
+import { IconButton} from '@mui/material'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { sideMenuChange, sideMenuSelector } from '../slices/slice'
