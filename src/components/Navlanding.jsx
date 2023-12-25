79bc29a1cd39689fcea79cba1a2aa835205f853c
ChandupaSH <chandupash107@gmail.com>
2023-12-25 18:42:04 +0530

    ad

--- src/components/Navlanding.jsx
+++ src/components/Navlanding.jsx
@@ -4,17 +4,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import { sideMenuChange, sideMenuSelector } from '../slices/slice'
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import CloseIcon from '@mui/icons-material/Close';
-// import PersonIcon from '@mui/icons-material/Person';
+import { motion,useScroll,  useTransform } from 'framer-motion'
 
 const Navlanding = () => {
   // const isTablet = useMediaQuery('(min-width: 500px )')
   const isClose = useSelector(sideMenuSelector)
   const dispatch = useDispatch()
 
+  const { scrollYProgress} = useScroll()
+  const opacity = useTransform(scrollYProgress,[0,0.3],[1, 0])
+  const width = useTransform(scrollYProgress,[0,0.2],[100, 120])
+
  
 
   return (
-    <div style={{width: '100%',margin: 0}}>
+    <motion.div style={{
+      margin: 0,
+      opacity: opacity,
+      width: '100%',
+     
+      }}>
 
         <img src='/nav_l_full.png' alt='m_nav_rect.png'
             style={{
@@ -36,7 +45,7 @@ const Navlanding = () => {
         </div>      
 
         
-    </div>
+    </motion.div>
   )
 }
 
